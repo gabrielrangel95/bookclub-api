672e90948947ff89808d5f6055e41d92ed431e8f
Gabriel Rangel <gabriel@sevenapps.tech>
2023-02-15 13:02:40 -0500

    fix: cors issue

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ dotenv.config();
 import express from "express";
 import db from "./models";
 import routes from "./routes";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors("*"));
 
 app.listen(3333, async () => {
   try {
